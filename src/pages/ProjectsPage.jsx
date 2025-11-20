bf3e9bbda7efca1ea5837655235ac55f4cf7c5c8
tunay-tuncer <tunayekin.tuncer@gmail.com>
2025-11-20 22:04:27 +0300

    new bg

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
 
 // Styles
@@ -12,10 +12,40 @@ const ProjectsPage = () => {
 
     const { user, isAuthenticated } = useAuth0();
 
+    const [projectsList, setprojectsList] = useState([
+        { projectName: "Şahika Hanım", furnitureCount: 2, totalPrice: 150000, color: "blue" },
+        { projectName: "Iraz Hanım", furnitureCount: 5, totalPrice: 20000, color: "red" },
+        { projectName: "Arif Bey", furnitureCount: 1, totalPrice: 450000, color: "green" }
+    ])
+
     return (
         <PageWrapper>
-            ProjectsPage
-            {isAuthenticated && <p>{JSON.stringify(user)}</p>}
+            {/* ProjectsPage */}
+            {/* {isAuthenticated && <p>{JSON.stringify(user)}</p>} */}
+
+            <h1>PROJECTS</h1>
+
+            <ul className={styles.projectsMainContainer}>
+                {projectsList.map((project, id) => (
+                    <li key={id} className={styles.projectContainer}>
+                        <div className={styles.clientNameDiv}>
+                            <p className={styles.descriptionText}>Müşteri</p>
+                            <p>{project.projectName}</p>
+                        </div>
+                        <div className={styles.furnitureCountDiv}>
+                            <p className={styles.descriptionText}>Mobilya Sayısı</p>
+                            <p>{project.furnitureCount}</p>
+                        </div>
+                        <div className={styles.totalPriceDiv}>
+                            <p className={styles.descriptionText}>Total Maliyet</p>
+                            <p>{project.totalPrice}</p>
+                        </div>
+                    </li>
+                ))}
+
+                <li className={styles.projectContainer}>Add Project</li>
+            </ul>
+
         </PageWrapper>
     );
 }
